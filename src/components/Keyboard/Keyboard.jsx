c5788077e6a6558e779b7dbb6dacc965af2ddb7d
AlphaDureku <templanzamark2002@gmail.com>
2023-05-26 16:02:16 +0800

    fixed keyboard layout

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -1,31 +1,38 @@
-/* eslint-disable react/prop-types */
 import "./Keyboard.css";
 
-const Keyboard = ({
+const KeyboardContainer = ({
   removeFromScreen = false,
-  activeLetters,
   inactiveLetters,
+  activeLetters,
   addGuessedLetter,
 }) => {
-  return (
-    <>
-      {!removeFromScreen &&
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map((letter) => {
-          const isActive = activeLetters.includes(letter);
-          const isInactive = inactiveLetters.includes(letter);
-          return (
-            <button
-              value={letter}
-              key={letter}
-              className="button-alpha"
-              onClick={() => addGuessedLetter(letter)}
-              disabled={isInactive || isActive}
-            >
-              {letter}
-            </button>
-          );
-        })}
-    </>
-  );
+  const rows = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+    ["Z", "X", "C", "V", "B", "N", "M"],
+  ];
+
+  if (!removeFromScreen) {
+    return (
+      <div className="keyboard-container">
+        {rows.map((row, index) => (
+          <div key={index}>
+            {row.map((letter) => (
+              <button
+                key={letter}
+                className="button-alpha"
+                onClick={() => addGuessedLetter(letter)}
+                disabled={inactiveLetters.includes(letter)}
+              >
+                {letter}
+              </button>
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
+  return null;
 };
-export default Keyboard;
+
+export default KeyboardContainer;
