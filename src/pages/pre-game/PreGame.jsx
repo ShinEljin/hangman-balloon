f985127ee7af120fbcfac8300e95d34685b7dd50
philip <philipangeloang1@gmail.com>
2023-05-26 15:14:12 +0800

    Fixed pre-game loading screen

--- src/pages/pre-game/PreGame.jsx
+++ src/pages/pre-game/PreGame.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import "./PreGame.css";
+import "../main-screen/MainScreen.css";
 import { useNavigate } from "react-router-dom";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
@@ -7,35 +8,39 @@ function PreGame() {
   // PAG CLINIC YUNG YES SA ARE YOU SURE TSAKA LANG TO GAGAWIN
 
   return (
-    <div id="mains">
-      <div>
-        <div className="main-text">
-          Stage 1 <br />
-          Balloons: 5
-        </div>
-        <div className="main-text">
-          {/* {secondsLeft} */}
-          <CountdownCircleTimer
-            isPlaying
-            duration={5}
-            colors={[
-              "#00c127",
-              "#00c127",
-              "#F7B801",
-              "#F7B801",
-              "#A30000",
-              "#A30000",
-            ]}
-            colorsTime={[5, 4, 3, 2, 1, 0]}
-            strokeWidth={20}
-            strokeLinecap="square"
-            rotation="counterclockwise"
-          >
-            {renderTime}
-          </CountdownCircleTimer>
+    <>
+      <div className="dim_gray" />
+      <div className="cloud__class__1">
+        <div className="app-bg-pre-game">
+          <div className="main-text">
+            Stage 1 out of 5 <br />
+            <span className="balloon-text">Balloons: 5</span>
+          </div>
+          <div className="main-text">
+            {/* {secondsLeft} */}
+            <CountdownCircleTimer
+              isPlaying
+              duration={5}
+              colors={[
+                "#CFAA4B",
+                "#CFAA4B",
+                "#CFAA4B",
+                "#CFAA4B",
+                "#CFAA4B",
+                "#CFAA4B",
+              ]}
+              strokeWidth={20}
+              trailStrokeWidth={30}
+              trailColor="#4F3222"
+              strokeLinecap="square"
+              rotation="counterclockwise"
+            >
+              {renderTime}
+            </CountdownCircleTimer>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
