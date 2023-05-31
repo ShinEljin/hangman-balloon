e766d92ca2b10fc04b634b9662c47625a3637275
mrknoee <markfetero@gmail.com>
2023-05-31 19:00:27 +0800

    Merge upstream changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import MainScreen from "./pages/main-screen/MainScreen";
 import PreGame from "./pages/pre-game/PreGame";
-import GameScreen from "./pages/game-screen/GameScreen";
+import SoundProvider from "./SoundContext";
 
 function App() {
   return (
+
+    <SoundProvider>
     <Routes>
       <Route path="/" element={<MainScreen />} />
       <Route path="/pre-game" element={<PreGame />} />
       <Route path="/game" element={<GameScreen />} />
     </Routes>
+    </SoundProvider>
+
   );
 }
 
