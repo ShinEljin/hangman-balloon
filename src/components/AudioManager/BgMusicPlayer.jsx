191f4585d5f7fa95593c5ef10b5503b2da5ce97d
shineljin <psyruz18@gmail.com>
2023-06-01 21:23:29 +0800

    fix

--- src/components/AudioManager/BgMusicPlayer.jsx
+++ src/components/AudioManager/BgMusicPlayer.jsx
@@ -1,34 +1,35 @@
 import { useEffect, useRef } from "react";
 import useSound from "use-sound";
+
 import Home from "../../assets/soundFx/Homebgmusic.mp3";
 import PreGame from "../../assets/soundFx/Pregame.mp3";
-import InGame from "../../assets/soundFx/IngameBG.mp3";
+import InGame from "../../assets/soundFx/InGameBG.mp3";
 
 const soundSources = {
   Home: {
     src: Home,
-    volume: 0.3
+    volume: 0.3,
   },
   PreGame: {
     src: PreGame,
-    volume: 0.5
+    volume: 0.5,
   },
   InGame: {
     src: InGame,
-    volume: 0.2
+    volume: 0.2,
   },
   Remove: {
     src: "",
-  }
+  },
 };
 
 export default function BgMusicPlayer({ isMusicEnabled, musicId }) {
-  const { src, volume } = soundSources[musicId] || {}; 
+  const { src, volume } = soundSources[musicId] || {};
 
   const [play, { pause }] = useSound(src, {
     volume,
     loop: true,
-    interrupt: true
+    interrupt: true,
   });
 
   const currentMusicIdRef = useRef(null);
@@ -43,18 +44,17 @@ export default function BgMusicPlayer({ isMusicEnabled, musicId }) {
   useEffect(() => {
     if (isMusicEnabled) {
       if (currentMusicIdRef.current && currentMusicIdRef.current !== musicId) {
-        pause(); 
+        pause();
       }
-      currentMusicIdRef.current = musicId; 
+      currentMusicIdRef.current = musicId;
       play();
     } else {
       pause();
     }
 
-
     return () => {
       pause();
-      currentMusicIdRef.current = null; 
+      currentMusicIdRef.current = null;
     };
   }, [isMusicEnabled, musicId, play, pause]);
 
